ce7490e287b5c66a01f34c0175590af347f18f32
kz375 <52p1314kevin@gmail.com>
2026-09-24 22:33:23 -0700

    Add files via upload

--- web/public/realtime-test/campaign.js
+++ web/public/realtime-test/campaign.js
@@ -1,6 +1,6 @@
 "use strict";
 
-globalThis.ABYSS_BETA_BUILD = "2.10.0";
+globalThis.ABYSS_BETA_BUILD = "2.10.1";
 
 const HERO_ART = Object.freeze({
   warrior:"/assets/characters/warrior.png", mage:"/assets/characters/mage.png",
